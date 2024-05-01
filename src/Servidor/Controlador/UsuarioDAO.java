fbb3168cefd21b6b54655ef3f0af49977cdcce19
AndresRuiz14502 <andresruiz14502@gmail.com>
2024-04-30 19:40:32 -0500

    arreglando los lloros de Cesar

--- src/Servidor/Controlador/UsuarioDAO.java
+++ src/Servidor/Controlador/UsuarioDAO.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Cliente.Controlador;
+package Servidor.Controlador;
 
-import Cliente.Modelo.Conexion;
+import Servidor.Modelo.Conexion;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
